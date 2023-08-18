53746817a653a2aff07aaade2ebffc73aa548769
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-18 13:33:50 +0000

    add files

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 TOKEN: Final = '6454077984:AAHqNy50ZKN-daZvmUYDr_Z2ymdBUmNk3bk'
 BOT_USERNAME: Final = '@kievairalarm_bot'
 
-async def start_command()
+#commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Thanks for chatting with me! I am a kiev air alarm bot!')
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Please type something so that I can respond')
+
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! This is a custom command')    
+
+
+
+#responses
+    
