ce3c32c0a8d5b74fb1072a4585a72d4287548f88
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-19 21:12:37 +0000

    pylint

--- main.py
+++ main.py
@@ -21,21 +21,14 @@ from python_telegram_messager import start_command,help_command,custom_command,h
 #PYTHON TELEGRAM MESSAGER
 print('Starting the bot...')
 app = Application.builder().token(TOKEN).build()
-
-
 #Commands
 app.add_handler(CommandHandler('start', start_command))
 app.add_handler(CommandHandler('help', help_command))
 app.add_handler(CommandHandler('custom', custom_command))
-
-
 #Messages
 app.add_handler(MessageHandler(filters.TEXT, handle_message))
-                
-
 #Errors
 app.add_error_handler(error)
-
 #Polls the bot
 print('Polling...')
 app.run_polling(poll_interval=3)
--- python_telegram_messager.py
+++ python_telegram_messager.py
@@ -1,3 +1,4 @@
+"""module for telegram messager in python"""
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
@@ -11,19 +12,24 @@ BOT_USERNAME: Final = '@kievairalarm_bot'
 
 #errors
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """fnxn to handle errors"""
     print(f'Update {update} caused error {context.error}')
 
 #commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """fnxn when /start is typed"""
     await update.message.reply_text('Hello! Thanks for chatting with me! I am a kiev air alarm bot!')
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """fnxn when /help is typed"""
     await update.message.reply_text('Hello! Please type something so that I can respond')
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """fnxn when /custom is typed"""
     await update.message.reply_text('Hello! This is a custom command')    
 
 #responses
 def handle_response(text: str) -> str:
-    processed: str = text.lower()   
+    """fnxn to handle message text"""
+    processed: str = text.lower()
     if 'hello' in processed:
         return 'Hey there!' 
     if 'how are you' in processed:
@@ -34,6 +40,7 @@ def handle_response(text: str) -> str:
 
 #handle message
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """fnxn to handle message from user"""
     message_type: str = update.message.chat.type
     text: str = update.message.text
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
@@ -46,6 +53,5 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         response: str = handle_response(text)
     print('Bot:', response)
-    asyncio.run(check_for_alarm())
+    #asyncio.run(check_for_alarm())
     await update.message.reply_text(response)
-
