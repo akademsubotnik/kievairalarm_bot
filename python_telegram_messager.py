00c4906b7160e8f96242a87053dce7a2cb7e30e1
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-19 23:58:51 +0300

    Update python_telegram_messager.py

--- python_telegram_messager.py
+++ python_telegram_messager.py
@@ -9,42 +9,34 @@ from check_for_alarms import check_for_alarm
 TOKEN: Final = '6454077984:AAHqNy50ZKN-daZvmUYDr_Z2ymdBUmNk3bk'
 BOT_USERNAME: Final = '@kievairalarm_bot'
 
+#errors
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
 #commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! Thanks for chatting with me! I am a kiev air alarm bot!')
-
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! Please type something so that I can respond')
-
-
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! This is a custom command')    
 
-
-
 #responses
 def handle_response(text: str) -> str:
-    processed: str = text.lower()
-    
+    processed: str = text.lower()   
     if 'hello' in processed:
-        return 'Hey there!'
-    
+        return 'Hey there!' 
     if 'how are you' in processed:
         return 'I am good thank you!'
-
     if 'i love python' in processed:
         return 'Remember to subscribe!'
-
     return 'I do not understand what you wrote...'
 
-
+#handle message
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
-
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
-
     if message_type == 'group':
         if BOT_USERNAME in text:
             new_text: str = text.replace(BOT_USERNAME, '').strip()
@@ -53,33 +45,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             return
     else:
         response: str = handle_response(text)
-
     print('Bot:', response)
     await update.message.reply_text(response)
 
-
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
-
-#MAIN
-# if __name__ == '__main__':
-#     print('Starting the bot...')
-#     app = Application.builder().token(TOKEN).build()
-
-
-#     #Commands
-#     app.add_handler(CommandHandler('start', start_command))
-#     app.add_handler(CommandHandler('help', help_command))
-#     app.add_handler(CommandHandler('custom', custom_command))
-
-
-#     #Messages
-#     app.add_handler(MessageHandler(filters.TEXT, handle_message))
-                    
-    
-#     #Errors
-#     app.add_error_handler(error)
-
-#     #Polls the bot
-#     print('Polling...')
-#     app.run_polling(poll_interval=3)
