40504125b908517c745981801ad7aa5f005d75ba
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-20 16:16:24 +0000

    prune main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 """"main.py"""
-#python messager
-from typing import Final
-from telegram import Update
+
 from telegram.ext import (
     Application,
     CommandHandler,
     MessageHandler,
     filters,
-    ContextTypes,
-    JobQueue,
+    #ContextTypes,
 
 )
 from python_telegram_messager import (
@@ -21,20 +18,10 @@ from python_telegram_messager import (
     callback_minute
 )
 
-#air alarms
-import asyncio
-from check_for_alarms import check_for_alarm
-
-#import time
-import time
 
 ######
 #MAIN#
 ######
-
-#ALARM CODE
-#while True:
-#asyncio.run(check_for_alarm())
 
 #PYTHON TELEGRAM MESSAGER
 print('Starting the bot...')
@@ -43,20 +30,16 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler('start', start_command))
 app.add_handler(CommandHandler('help', help_command))
 app.add_handler(CommandHandler('custom', custom_command))
+
 #Messages
 app.add_handler(MessageHandler(filters.TEXT, handle_message))
 #Errors
 app.add_error_handler(error)
-# #Polls the bot
-# print('Polling...')
-# app.run_polling(poll_interval=3)
+
 #Job Queue
 job_queue = app.job_queue
-job_minute = job_queue.run_repeating(callback_minute, interval=60, first=10)
+job_minute = job_queue.run_repeating(callback_minute, interval=5, first=10)
 
+# #Polls the bot
+# print('Polling...')
 app.run_polling()
-
-# while True:
-
-#     time.sleep(5)
-
