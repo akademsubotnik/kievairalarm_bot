84d1bfe70e62cea59891c5d0c8e397fb98705178
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-20 16:41:52 +0000

    final pruning???

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_error_handler(error)
 
 #Job Queue
 job_queue = app.job_queue
-job_minute = job_queue.run_repeating(callback_minute, interval=5, first=10)
+job_minute = job_queue.run_repeating(callback_minute, interval=60, first=10)
 
 # #Polls the bot
 # print('Polling...')
--- python_telegram_messager.py
+++ python_telegram_messager.py
@@ -18,31 +18,6 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """fnxn to handle errors"""
     print(f'Update {update} caused error {context.error}')
 
-#commands
-# async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     """fnxn when /start is typed"""
-#     await update.message.reply_text('Hello! Thanks for chatting with me! I am a kiev air alarm bot!')
-# async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     """fnxn when /help is typed"""
-#     await update.message.reply_text('Hello! Please type something so that I can respond')
-# async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     """fnxn when /custom is typed"""
-#     await update.message.reply_text('Hello! This is a custom command')
-
-
-#handle message
-# async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     """fnxn to handle message from user"""
-
-#     nest_asyncio.apply()
-#     loop = asyncio.get_event_loop()
-#     alarm = loop.run_until_complete(asyncio.ensure_future(check_for_alarm()))
-#     if alarm == True:
-#         await update.message.reply_text("THERE IS AN AIR ALARM IN KIEV CITY!")
-#     if alarm == False:
-#         await update.message.reply_text("there is no air alarm in kiev city")
-
-
 async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
     """"fnxn to send messages to TG channel on timer"""
     nest_asyncio.apply()
