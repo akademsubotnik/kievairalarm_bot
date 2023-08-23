070740b8ae83c331a33382fd25de3ece78624f29
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-23 19:35:29 +0000

    read from public channels to std.out

--- read-public-channels/telethon-main.py
+++ read-public-channels/telethon-main.py
@@ -1,13 +1,13 @@
 from telethon import TelegramClient
 
 # Remember to use your own values from my.telegram.org!
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+api_id = 16451568
+api_hash = '0e22fa1aaca1ef1bc7feba02f6f53622'
 client = TelegramClient('anon', api_id, api_hash)
 
 async def main():
     # You can print the message history of any chat:
-    async for message in client.iter_messages('USERNAME OF THE CHANNEL'):
+    async for message in client.iter_messages('air_alert_ua'):
         print(message.sender.username, message.text)
 
 with client:
