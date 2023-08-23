634698153d8c3ad4e0d62d6ff5361a837a0d06b1
greg <gregjsmith@gmx.com>
2023-08-24 01:16:39 +0300

    update code

--- read-public-channels/telethon-main.py
+++ read-public-channels/telethon-main.py
@@ -11,8 +11,10 @@ async def get_latestmessage():
     # You can print the message history of any chat:
     #this prints posts with latest post first
     #IDEA --- Get latest message and set it to a variable, every 5 seconds check if the message has chagned.  Is the message can changed set as the latest message and repeat!
-    async for message in client.iter_messages('thisis_kyiv'):
-        print(message.sender.username, message.text)
+    message = await client.get_messages('thisis_kyiv')
+    print(len(message))
+    #client.get_messages('thisis_kyiv')###UNDERSTAND HOW FUNCTION GET MESSAGES WORKS TO GET MULTIPLE MESSAGES!!!!!!!
+    #print(message[0].sender.username , message[0].text)
 
 with client:
     client.loop.run_until_complete(get_latestmessage())
