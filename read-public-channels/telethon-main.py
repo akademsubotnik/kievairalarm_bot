fd56312af267993e6ca7aafe4b3d6ca4a2b42a02
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-08-23 21:02:25 +0000

    update function names

--- read-public-channels/telethon-main.py
+++ read-public-channels/telethon-main.py
@@ -5,10 +5,14 @@ api_id = 16451568
 api_hash = '0e22fa1aaca1ef1bc7feba02f6f53622'
 client = TelegramClient('anon', api_id, api_hash)
 
-async def main():
+
+
+async def get_latestmessage():
     # You can print the message history of any chat:
-    async for message in client.iter_messages('air_alert_ua'):
+    #this prints posts with latest post first
+    #IDEA --- Get latest message and set it to a variable, every 5 seconds check if the message has chagned.  Is the message can changed set as the latest message and repeat!
+    async for message in client.iter_messages('thisis_kyiv'):
         print(message.sender.username, message.text)
 
 with client:
-    client.loop.run_until_complete(main())
+    client.loop.run_until_complete(get_latestmessage())
